3d5372a642b28472add5545b572337f5dd24eec1
Chris <chris@csie.io>
2020-08-05 19:59:30 +0800

    Implement cache and custom sorting function.

--- start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -16,6 +16,8 @@
 
 package com.example.android.advancedcoroutines
 
+import com.example.android.advancedcoroutines.util.CacheOnSuccess
+import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
@@ -33,6 +35,29 @@ class PlantRepository private constructor(
     private val plantService: NetworkService,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
+
+    /**
+     * plantsListSortOrderCache is used as the in-memory cache for the custom sort order.
+     * It will fallback to an empty list if there's a network error,
+     * so that our app can still display data even if the sorting order isn't fetched.
+     */
+    private var plantsListSortOrderCache =
+        CacheOnSuccess(onErrorFallback = { listOf<String>() }) {
+            plantService.customPlantSortOrder()
+        }
+
+    /**
+     * This extension function will rearrange the list,
+     * placing Plants that are in the customSortOrder at the front of the list.
+     */
+    private fun List<Plant>.applySort(customSortOrder: List<String>): List<Plant> {
+        return sortedBy { plant ->
+            val positionForItem = customSortOrder.indexOf(plant.plantId).let { order ->
+                if (order > -1) order else Int.MAX_VALUE
+            }
+            ComparablePair(positionForItem, plant.name)
+        }
+    }
 
     /**
      * Fetch a list of [Plant]s from the database.
@@ -94,7 +119,8 @@ class PlantRepository private constructor(
     companion object {
 
         // For Singleton instantiation
-        @Volatile private var instance: PlantRepository? = null
+        @Volatile
+        private var instance: PlantRepository? = null
 
         fun getInstance(plantDao: PlantDao, plantService: NetworkService) =
             instance ?: synchronized(this) {
--- start/src/main/java/com/example/android/advancedcoroutines/ui/MainActivity.kt
+++ start/src/main/java/com/example/android/advancedcoroutines/ui/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.example.android.advancedcoroutines.ui
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.example.android.advancedcoroutines.R
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
